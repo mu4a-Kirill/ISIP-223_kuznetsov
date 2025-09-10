372f7601224e8cd7df82b7784e627e6084308c51
Кирилл Кузнецов <mu4a1234@gmail.com>
2025-09-10 11:32:12 +0300

    ISIP223_KUZNETSOV

--- pr1/Program.cs
+++ pr1/Program.cs
@@ -70,6 +70,7 @@ namespace DailyExpenses
                         return;
                     default:
                         Console.WriteLine("Неверный выбор!");
+                    //
                         break;
                 }
             }
